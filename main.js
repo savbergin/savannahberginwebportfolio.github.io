1114834633690da8c283f140081dc3cb877e8d9d
savbergin <74926083+savbergin@users.noreply.github.com>
2020-12-16 14:53:00 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
    
-var bodyElement = document.querySelector('body');
-var menuToggle = document.querySelector('.hamburger-menu');
-var navList = document.querySelector('.nav-list');
-var heroSection = document.querySelector('.hero');
-
-});
+bodyElement = document.querySelector('body');
+menuToggle = document.querySelector('.hamburger-menu');
+navList = document.querySelector('.nav-list');
+heroSection = document.querySelector('.hero');
 
 //Add event listener to the hamburger menu to toggle nav on and off and switch icons
 menuToggle.addEventListener('click', function() {
@@ -15,7 +13,7 @@ menuToggle.addEventListener('click', function() {
 // Add event listener on the nav-list then grab the item clicked
 navList.addEventListener("click", function (e) {
     //Grabs the clicked element
-    var clickedElement = e.target;
+    if clickedElement = e.target;
     //If the clicked item has a class of nav-link at index 0 then do something
     if (clickedElement.classList[0] === "nav-link") {
         bodyElement.classList.remove('is-open');
@@ -29,7 +27,7 @@ the nav and remove the dark content overlay
 */
 window.addEventListener('click', function (e) {
     //Grabs the clicked element
-    var clickedElement = e.target;
+  if clickedElement = e.target;
     //If the clicked element matches the class then do something
     if (clickedElement.matches('.is-open')) {
         bodyElement.classList.remove('is-open');
